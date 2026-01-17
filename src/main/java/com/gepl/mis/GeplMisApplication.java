323fcab8a8fcd72b7cb09831231e3efac028f0c4
prashantsharmaknp8 <prashantsharmaknp8@gmail.com>
2026-01-17 15:12:55 +0530

    Initial commit on mis

---
+++ src/main/java/com/gepl/mis/GeplMisApplication.java
@@ -0,0 +1,13 @@
+package com.gepl.mis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeplMisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeplMisApplication.class, args);
+	}
+
+}
