5306538a86f66716000fe1c5c459e38aff9d014a
prashantsharmaknp8 <prashantsharmaknp8@gmail.com>
2026-01-29 15:21:05 +0530

    cors options commit2

--- src/main/java/com/gepl/mis/config/CorsHeaderFilter.java
+++ src/main/java/com/gepl/mis/config/CorsHeaderFilter.java
@@ -21,7 +21,7 @@ public class CorsHeaderFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
-        response.setHeader("Access-Control-Max-Age", "3600");
+
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
