06fd8da2a82dde83f0c7c4b7b2cf5bef67583d71
prashantsharmaknp8 <prashantsharmaknp8@gmail.com>
2026-01-31 11:23:00 +0530

    validation update in iventoryRequest commit

--- src/main/java/com/gepl/mis/inventory/InventoryRequest.java
+++ src/main/java/com/gepl/mis/inventory/InventoryRequest.java
@@ -1,12 +1,21 @@
 package com.gepl.mis.inventory;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class InventoryRequest {
+
+    @NotBlank
     private String itemCode;
+    @NotBlank
     private String itemName;
+    @Min(value = 1,message = "Received Quantity must be >=1")
     private Integer quantity;
+    @NotBlank
     private String unit;
+    @NotBlank
     private String location;
 }
