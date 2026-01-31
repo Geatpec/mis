32e27713a8e8c9d27516df907f163f3ef30386a7
prashantsharmaknp8 <prashantsharmaknp8@gmail.com>
2026-01-31 11:51:18 +0530

    validation update in projectRequest commit

--- src/main/java/com/gepl/mis/project/ProjectRequest.java
+++ src/main/java/com/gepl/mis/project/ProjectRequest.java
@@ -1,5 +1,8 @@
 package com.gepl.mis.project;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -7,12 +10,18 @@ import java.time.LocalDate;
 
 @Data
 public class ProjectRequest {
+    @NotBlank
     private String projectCode;
+    @NotBlank
     private String projectName;
+    @NotBlank
     private String clientName;
 
+    @NotNull
     private LocalDate plannedStartDate;
+    @NotNull
     private LocalDate plannedEndDate;
 
+    @DecimalMin(value = "0.01", message = "PlannedBudget must be greater than 0")
     private BigDecimal plannedBudget;
 }
